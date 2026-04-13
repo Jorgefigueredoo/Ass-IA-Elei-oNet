b8098870e87995491e854384bf5cff4ccbb13892
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-12 21:10:39 -0300

    ...

---
+++ eleicaonet-ai/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Cria um arquivo local chamado eleicaonet.db para armazenar os dados do banco de dados SQLite
+SQLALCHEMY_DATABASE_URL = "sqlite:///./eleicaonet.db"
+
+# connect_args={"check_same_thread": False} é necessário apenas para o SQLite
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
